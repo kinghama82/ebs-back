8e12fdb17c0244fcb653986db65fb68f012b6b55
kinghama82 <wngjsrb88@naver.com>
2025-02-05 12:31:36 +0900

    1st

---
+++ src/main/java/com/ebs/boardparadice/BoardparadiceApplication.java
@@ -0,0 +1,13 @@
+package com.ebs.boardparadice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardparadiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardparadiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ebs/boardparadice/BoardparadiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ebs.boardparadice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardparadiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
