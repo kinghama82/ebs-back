dd1bf1f43bfb79b296f6933f3d1428863b22b3de
3-06 <a39039107@gmail.com>
2025-02-13 15:23:48 +0900

    NoticeService 삭제

--- src/test/java/com/ebs/boardparadice/BoardparadiceApplicationTests.java
+++ src/test/java/com/ebs/boardparadice/BoardparadiceApplicationTests.java
@@ -1,13 +1,22 @@
 package com.ebs.boardparadice;
 
+import com.ebs.boardparadice.model.boards.Notice;
+import com.ebs.boardparadice.repository.BoardTypeRepository;
+import com.ebs.boardparadice.repository.boards.NoticeRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
 
 @SpringBootTest
 class BoardparadiceApplicationTests {
 
+
 	@Test
 	void contextLoads() {
-	}
 
-}
+
+		}
+
+	}
