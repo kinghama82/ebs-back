77635368ef92151d32c153b7da0e06a0f2cf74f4
김정호 <kimwjdgh@gmail.com>
2025-02-26 16:31:48 +0900

    회원가입, 로그인완료

--- src/main/java/com/ebs/boardparadice/controller/LocalDateFormatter.java
+++ src/main/java/com/ebs/boardparadice/controller/LocalDateFormatter.java
@@ -8,6 +8,7 @@ import java.util.Locale;
 
 public class LocalDateFormatter implements Formatter<LocalDate> {
 
+
     @Override
     public LocalDate parse(String text, Locale locale) {
         return LocalDate.parse(text, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
